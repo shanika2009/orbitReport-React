aa081d9bd782f1c0f7309664bc727e6dd230cdbe
Shanika <shanika.stovall@gmail.com>
2023-07-27 20:26:34 -0500

    update code

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import "./styling.css" 
+
+export default function Banner () {
+    return (
+        <div>
+            <h1>Obrit Report</h1>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
